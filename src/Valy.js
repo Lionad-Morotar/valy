c0a68bf92f884902d4c2e6713b0e200b2aa39ad6
Lionad <tangnad@qq.com>
2020-08-18 01:58:41 +0800

    refactor: ...

--- src/Valy.js
+++ src/Valy.js
@@ -1,53 +1,7 @@
-'use strict';
-
-// import utils from './utils'
-
-var defaultValidator = {
-
-  /** social media */
-
-  email: /^([A-Za-z0-9_.])+@([A-Za-z0-9_])+.([A-Za-z]{2,6})$/,
-  mobile: /^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(18[0,5-9]))\d{8}$/,
-
-  /** number */
-
-  interger: /^-?\d+$/,
-  float: /^-?\d*.\d*$/,
-  number: 'interger||float',
-  max (max) {
-    return ['notNaN', +this.value < max]
-  },
-  min (min) {
-    return ['notNaN', +this.value > min]
-  },
-
-  /** array */
-  
-  some (_) { 
-    return this.value.some(x => this._valid(_, { value: x })) 
-  },
-  every (_) { 
-    return this.value.every(x => this._valid(_, { value: x })) 
-  },
-
-  /** general */
-
-  required: /.+/,
-  is (_) { 
-    return this.value === _ 
-  },
-  not (_) { 
-    return this.value !== _ 
-  },
-  has (_) {
-    return this.value.includes(_)
-  }
-};
-
 /**
  * Valy 链式调用的校验器 
  */
-class Valy {
+export default class Valy {
 
   /** Constructor
    * @param {Any} value 待校验的值
@@ -64,20 +18,20 @@ class Valy {
       result: null,
       message: '',
       value
-    });
+    })
 
     const insiderProps = Object.keys(this)
-      .concat(['_valid','format','valid','flush']);
+      .concat(['_valid','format','valid','flush'])
 
     return new Proxy(this, {
       get: (target, prop, receiver) => {
         if (insiderProps.includes(prop)) {
           return Reflect.get(target, prop, receiver)
         } else {
-          const handle = fns.find(x => x.has(prop));
-          const fn = handle.get(prop);
+          const handle = fns.find(x => x.has(prop))
+          const fn = handle.get(prop)
           return params => {
-            this.valid(fn.bind ? fn.bind(this, params) : fn);
+            this.valid(fn.bind ? fn.bind(this, params) : fn)
             return receiver
           } 
         }
@@ -90,21 +44,21 @@ class Valy {
    * @param options.stragedy 当校验对象是数组时，默认使用 and（every）还是 some 逻辑
    */
   _valid (validators = [], options = { value: this.value }) {
-    options = Object.assign({ stragedy: 'and' }, options);
-    const value = options.value;
+    options = Object.assign({ stragedy: 'and' }, options)
+    const value = options.value
 
     /* 根据校验器的类型执行校验策略 */
 
     const methods = {
       Function () {
-        const fnRes = validators(value);
-        const type = typeof fnResult;
+        const fnRes = validators(value)
+        const type = typeof fnResult
         return (type === 'function' || type === 'object')
           ? this._valid(fnRes)
           : fnRes
       },
       Array () {
-        const results = validators.map(x => this._valid(x));
+        const results = validators.map(x => this._valid(x))
         return options.stragedy === 'and'
           ? results.every(x => x === true)
           : results.some(x => x === true)
@@ -117,31 +71,31 @@ class Valy {
       },
       // TODO: '1&&2||(2||3)'
       String () {
-        let validResult = null;
-        const validArr = validators.split('||');
+        let validResult = null
+        const validArr = validators.split('||')
         if (validArr.length > 1) {
-          validResult = this._valid(validArr, Object.assign(options, { stragedy: 'or' }));
+          validResult = this._valid(validArr, Object.assign(options, { stragedy: 'or' }))
         } else {
-          const toFindHandle = validators.split('?');
-          const [fnName, params] = [toFindHandle[0], (toFindHandle[1] || '').split(',')];
-          const handle = fns.find(x => x.has(fnName)).get(fnName);
+          const toFindHandle = validators.split('?')
+          const [fnName, params] = [toFindHandle[0], (toFindHandle[1] || '').split(',')]
+          const handle = fns.find(x => x.has(fnName)).get(fnName)
           if (!handle) {
-            validResult = false;
-            this.message = validators;
+            validResult = false
+            this.message = validators
             return validResult
           }
           if (typeof handle === 'function') {
-            const handleFnRes = handle.bind(this)(...params);
-            validResult = this._valid(handleFnRes);
+            const handleFnRes = handle.bind(this)(...params)
+            validResult = this._valid(handleFnRes)
           } else {
-            validResult = this._valid(handle);
+            validResult = this._valid(handle)
           }
         }
         return validResult
       }
-    };
+    }
 
-    const validatorsType = Object.prototype.toString.call(validators).slice(8, -1);
+    const validatorsType = Object.prototype.toString.call(validators).slice(8, -1)
 
     if (!methods[validatorsType]) {
       throw new Error('Unsupported Validators Type')
@@ -154,21 +108,21 @@ class Valy {
   format (fnOrVal) {
     this.value = fnOrVal instanceof Function
       ? fnOrVal(this.value)
-      : fnOrVal;
+      : fnOrVal
     return this
   }
   
   // 同步校验
   valid (validators, opts) {
     if (this.pass) return this
-    this.pass = !(this.result = this._valid(validators, opts));
+    this.pass = !(this.result = this._valid(validators, opts))
     return this
   }
 
   // 异步校验
   async validAsync (validators, opts) {
     if (this.pass) return this
-    this.pass = !(this.result = this._valid(await validators, opts));
+    this.pass = !(this.result = this._valid(await validators, opts))
     return this
   }
 
@@ -181,15 +135,10 @@ class Valy {
 
 /* 通过 Valy.use 注册插件 */
 
-const fns = [];
+const fns = []
 Valy.use = models => {
-  const fnsMap = new Map(Object.entries(models));
-  fns.unshift(fnsMap);
-};
+  const fnsMap = new Map(Object.entries(models))
+  fns.unshift(fnsMap)
+}
 
 // TODO 有没有必要加入 Valy.destory 注销某个插件这种功能
-
-Valy.use(defaultValidator);
-
-module.exports = Valy;
-//# sourceMappingURL=index.js.map
--- test/index.js
+++ test/index.js
@@ -11,62 +11,18 @@ log(
   require('./every-some')
 )
 
-// log(new Valy().valid('required').getRes())
-
-// log(new Valy(0).valid('required').getRes())
-// log(new Valy('').valid('required').getRes())
-// log(new Valy('test').valid('required').getRes())
-
-// line()
-
-// log(new Valy(55.01).valid('max?max=55.02').getRes())
-// log(new Valy(55.01).valid('max?max=55.009').getRes())
-
-// line()
-
-// log(new Valy(55.01).valid(['max?max=55.011', 'min?min=49', _ => _ === 55.01]).getRes())
-
-// line()
-
-// log(new Valy(55.01).valid(['max?max=55.011', 'min?min=49', '数字不为55']).getRes())
-
-// line()
-
-/** async */
-
-// new Valy('5')
-//     .valid(/5/)
-//     .flush(),
-// new Valy('5')
-//   .validAsync(task().then(() => 'is?5'))
-//   .then(res => console.log(res.flush()))
-
-Valy.use({
-  max56: 'max?55',
-  min56: 'min?56',
-  maxminRequired: 'max56||min56||required'
-})
-
-const task = () => new Promise(resolve => {
-  setTimeout(() => {
-    resolve(1)
-  }, 1000)
-})
+// const task = () => new Promise(resolve => {
+//   setTimeout(() => {
+//     resolve(1)
+//   }, 1000)
+// })
 
 log(
-  // new Valy(55.01)
-  //   .not(49)
-  //   .required()
-  //   .number()
-  //   .min(55)
-  //   .max(60)
-  //   .maxminRequired()
-  //   .valid(/^55.01$/)
-  //   .valid([_ => [ _ => [ _ => _ < 56 ] ]])
-  //   .format(_ => 55)
-  //   .valid(/^55$/)
-  //   .format(_ => 1)
-  //   .valid(setTimeout(() => 'is?1', 1))
-  //   .flush(),
-  
+  new Valy(55.01)
+    .not(49)
+    .required()
+    .number()
+    .min(55)
+    .max(60)
+    .flush(),
 )
